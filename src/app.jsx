0b84034dae274577458327bc624051243b65eed5
Kevin Qi <iqnivek@gmail.com>
2016-01-10 23:18:22 -0500

    Initial scaffolding for a react starter project

---
+++ src/app.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Your content here</div>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: "./src/app"
+  },
+  output: {
+    path: "./dist",
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+    modulesDirectories: ["node_modules"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel",
+        exclude: [__dirname + "/node_modules"],
+        query: {
+          presets: ["es2015", "react"],
+          cacheDirectory: true
+        }
+      }
+    ]
+  }
+};
